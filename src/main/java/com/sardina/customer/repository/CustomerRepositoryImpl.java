8265712e36588776d13ca92240dca18513181b92
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-10-04 17:20:28 -0400

    PROJ Spring Yard: #1: Structure set up. TODO's: implement interfaces and write tests for service.

--- src/main/java/com/sardina/customer/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/sardina/customer/repository/CustomerRepositoryImpl.java
@@ -1,20 +1,26 @@
 package com.sardina.customer.repository;
 
 import com.sardina.customer.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public void add(Customer customer) {
-
+        //TODO: add(Customer c)
     }
 
     @Override
     public void update(Customer customer) {
-
+        //TODO: update(Customer c)
     }
 
     @Override
@@ -29,6 +35,6 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public void delete(int id) {
-
+        //TODO: delete(int i)
     }
 }
--- src/main/java/com/sardina/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/sardina/customer/service/CustomerServiceImpl.java
@@ -1,19 +1,26 @@
 package com.sardina.customer.service;
 
 import com.sardina.customer.model.Customer;
+import com.sardina.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
 
     @Override
     public void add(Customer customer) {
-
+        //TODO: add(Customer c)
     }
 
     @Override
     public void update(Customer customer) {
-
+        //TODO: update(Customer c)
     }
 
     @Override
@@ -28,6 +35,6 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public void delete(int id) {
-
+        //TODO: delete(int i)
     }
 }
