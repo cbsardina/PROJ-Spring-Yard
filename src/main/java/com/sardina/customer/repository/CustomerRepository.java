d22393615409585440c6dafb6454b9df4cd99e42
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-10-10 18:11:16 -0400

    PROJ Spring Yard: #3 - Rewrote all code and still getting the same error.

--- src/main/java/com/sardina/customer/repository/CustomerRepository.java
+++ src/main/java/com/sardina/customer/repository/CustomerRepository.java
@@ -1,15 +1,10 @@
 package com.sardina.customer.repository;
 
 import com.sardina.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CustomerRepository {
-
-    void add(Customer customer);
-    void update(Customer customer);
-    Customer getById(int id);
-    List<Customer> getAll();
-    void delete(int id);
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
--- src/main/java/com/sardina/customer/service/CustomerService.java
+++ src/main/java/com/sardina/customer/service/CustomerService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface CustomerService {
 
-    void add(Customer customer);
+    Customer add(Customer customer);
     void update(Customer customer);
     Customer getById(int id);
     List<Customer> getAll();
